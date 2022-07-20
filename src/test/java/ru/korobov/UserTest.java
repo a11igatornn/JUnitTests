f431819b2e8b0170e1c8f5edaef3d50dfee78532
a11igatornn <cgs-nn@mail.ru>
2022-07-20 15:21:38 +0300

    test 1

--- src/test/java/ru/korobov/UserTest.java
+++ src/test/java/ru/korobov/UserTest.java
@@ -1,6 +1,10 @@
 package ru.korobov;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +12,31 @@ public class UserTest {
 
     @Test
     public void getAllUsers() {
+        //создаем тестовые данные
+        User user = new User("Евгений", 35, Sex.MALE);
+        User user1 = new User("Марина", 34, Sex.FEMALE);
+        User user2 = new User("Алина", 7, Sex.FEMALE);
+
+        //создаем список expected и заполняем его данными нашего метода
+        List<User> expected = User.getAllUsers();
+
+        //создаем список actual в него помещаем данные для сравнения
+        //то что мы предпологиаем метод должен вернуть
+        List<User> actual = new ArrayList<>();
+        actual.add(user);
+        actual.add(user1);
+        actual.add(user2);
+
+        //запускаем тест, в случае если список expected и actual не будут равны
+        //тест будет провален, о результатах теста читаем в консоли
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAllUsers_NO_NULL() {
+        //добавим проверку на null
+        List<User> expected = User.getAllUsers();
+        Assert.assertNotNull(expected);
     }
 
     @Test
