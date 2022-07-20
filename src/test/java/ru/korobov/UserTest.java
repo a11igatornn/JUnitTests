1404590a5a9d93fb4c99eebe01b3738fb56021ac
a11igatornn <cgs-nn@mail.ru>
2022-07-20 15:12:21 +0300

    test create

---
+++ src/test/java/ru/korobov/UserTest.java
@@ -0,0 +1,40 @@
+package ru.korobov;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void getAllUsers() {
+    }
+
+    @Test
+    public void testGetAllUsers() {
+    }
+
+    @Test
+    public void getHowManyUsers() {
+    }
+
+    @Test
+    public void testGetHowManyUsers() {
+    }
+
+    @Test
+    public void getAllAgeUsers() {
+    }
+
+    @Test
+    public void testGetAllAgeUsers() {
+    }
+
+    @Test
+    public void getAverageAgeOfAllUsers() {
+    }
+
+    @Test
+    public void testGetAverageAgeOfAllUsers() {
+    }
+}
